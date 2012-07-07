15df4b0e8bcf68589a8ecd206c4aee780429f3f7
Mat Ryer <matryer@mac.com>
2012-07-07 20:53:17 +0100

    added comments and doc

---
+++ v1/stretchr/doc.go
@@ -0,0 +1,3 @@
+/*
+Package stretchr-go/v1/stretchr provides access to Stretchr services from Go code.
+*/
--- v1/stretchr/stretchr.go
+++
@@ -1,4 +0,0 @@
-// stretchr is the package for accessing Stretchr services from a Go app.
-//
-// See full documentation at: http://go.pkgdoc.org/github.com/stretchrcom/stretchr-go/stretchrcom/v1/stretchr
-package stretchr
