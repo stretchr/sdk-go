5ab6828cac6cee60e47a27cfe436e22bd34dd230
Mat Ryer <matryer@mac.com>
2012-07-07 20:45:11 +0100

    moved files around

--- stretchrcom/v1/stretchr/resources.go
+++
@@ -1,7 +0,0 @@
-package stretchr
-
-type Resource map[string]interface{}
-
-func MakeResource() Resource {
-	return make(Resource)
-}
--- stretchrcom/v1/stretchr/stretchr.go
+++
@@ -1,2 +0,0 @@
-// stretchr is the package for accessing Stretchr services from a Go app.
-package stretchr
