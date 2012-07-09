805345d60694aeef967954dc88926c798644a1a3
Mat Ryer <matryer@mac.com>
2012-07-09 17:59:51 +0100

    added docs

--- v0.2/stretchr/session_read.go
+++ v0.2/stretchr/session_read.go
@@ -1,5 +1,9 @@
 package stretchr
 
+// Read reads a single resource from Stretchr, based on the specified path and ID values.
+//
+// The following code will read the person with ID "123"
+//  resource, err := session.Read("people", "123")
 func (s *Session) Read(path, id string) (*Resource, error) {
 
 	resource := s.MakeResource(path).SetID(id)
