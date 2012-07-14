80ba4e8f53907cac34efc4b66669fa37e376d02b
Mat Ryer <matryer@mac.com>
2012-07-13 22:50:37 -0600

    removed 'log' statement

--- src/v0.2/stretchr/requester.go
+++ src/v0.2/stretchr/requester.go
@@ -2,7 +2,6 @@ package stretchr
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strings"
 )
@@ -72,8 +71,6 @@ func (r *DefaultRequester) MakeRequest(method, fullUrl, body, publicKey, private
 
 	// get the StandardResponseObject
 	sro, sroErr := ExtractStandardResponseObject(response)
-
-	log.Printf("error: %s", sroErr)
 
 	if sroErr != nil {
 		return nil, response, sroErr
