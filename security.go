1113860014578a348de52c91649ce297ad731b20
Tyler Bunnell <tylerbunnell@gmail.com>
2012-12-20 10:54:34 -0700

    Add security/signing

---
+++ security.go
@@ -0,0 +1,101 @@
+package stretchr
+
+import (
+	"crypto/sha1"
+	"fmt"
+	"net/url"
+	"sort"
+	"strings"
+)
+
+/*
+	Request Signing. See http://aws.amazon.com/articles/1928#HTTP for an idea of
+	how this works.
+*/
+
+const (
+	// FailedSignature is the string that is returned when signing fails.
+	FailedSignature string = "(failed signature)"
+)
+
+// hash generates an SHA1 hash of the specified string.
+func hash(s string) string {
+
+	hash := sha1.New()
+	hash.Write([]byte(s))
+	return fmt.Sprintf("%x", hash.Sum(nil))
+
+}
+
+// getOrderedParams gets the parameters ordered by key, then by values.
+func getOrderedParams(values url.Values) string {
+
+	// get the keys
+	var keys []string
+	for k, _ := range values {
+		keys = append(keys, k)
+	}
+
+	// sort the keys
+	sort.Strings(keys)
+
+	// ordered items
+	var ordered []string
+
+	// sort the values
+	for _, key := range keys {
+		sort.Strings(values[key])
+		for _, val := range values[key] {
+			ordered = append(ordered, fmt.Sprintf("%s=%s", url.QueryEscape(key), url.QueryEscape(val)))
+		}
+	}
+
+	joined := strings.Join(ordered, "&")
+	return joined
+
+}
+
+// getSignature gets the signature of a request based on the given parameters.
+func getSignature(method, requestUrl, body, privateKey string) (string, error) {
+
+	// parse the URL
+	u, parseErr := url.ParseRequestURI(requestUrl)
+
+	if parseErr != nil {
+		return FailedSignature, parseErr
+	}
+
+	// get the query values
+	values := u.Query()
+
+	// add the private key parameter
+	values.Set(signPrivateKey, privateKey)
+
+	if len(body) > 0 {
+		values.Set(signBodyHash, hash(body))
+	}
+
+	// get the ordered params
+	orderedParams := getOrderedParams(values)
+
+	base := strings.Split(u.String(), "?")[0]
+	combined := fmt.Sprintf("%s&%s?%s", strings.ToUpper(method), base, orderedParams)
+
+	return hash(combined), nil
+
+}
+
+// GetSignedURL gets the URL with the sign parameter added based on the given parameters.
+func GetSignedURL(method, requestUrl, body, privateKey string) (string, error) {
+
+	hash, hashErr := getSignature(method, requestUrl, body, privateKey)
+
+	if hashErr != nil {
+		return FailedSignature, hashErr
+	}
+
+	signed := fmt.Sprintf("%s&%s=%s", requestUrl, url.QueryEscape(signSignature), url.QueryEscape(hash))
+
+	return signed, nil
+
+}
---
+++ security_test.go
@@ -0,0 +1,67 @@
+package stretchr
+
+import (
+	"github.com/stretchrcom/testify/assert"
+	"net/url"
+	"testing"
+)
+
+func TestHash(t *testing.T) {
+
+	str := "abc"
+
+	assert.Equal(t, "a9993e364706816aba3e25717850c26c9cd0d89d", hash(str), "MAIN Hash test")
+
+}
+
+func TestGetOrderedParams(t *testing.T) {
+
+	values := make(url.Values)
+	values.Add("~key", "ABC123")
+	values.Add(":name", "!Mat")
+	values.Add(":name", "!Laurie")
+	values.Add(":age", ">20")
+	values.Add(":something", ">2 0")
+
+	ordered := getOrderedParams(values)
+
+	assert.Equal(t, "%3Aage=%3E20&%3Aname=%21Laurie&%3Aname=%21Mat&%3Asomething=%3E2+0&~key=ABC123", ordered)
+
+}
+
+func TestgetSignature(t *testing.T) {
+
+	var signed string
+
+	signed, _ = getSignature(HttpMethodGet, "http://test.stretchr.com/api/v1?~key=ABC123&:name=!Mat&:name=!Laurie&:age=>20", "body", "ABC123-private")
+	assert.Equal(t, "df073ee4086eed5848d167871c7424937027728e", signed)
+
+	signed, _ = getSignature("get", "http://test.stretchr.com/api/v1?~key=ABC123&:name=!Mat&:name=!Laurie&:age=>20", "body", "ABC123-private")
+	assert.Equal(t, "df073ee4086eed5848d167871c7424937027728e", signed, "Lower case method shouldn't affect GetSignature")
+
+	signed, _ = getSignature(HttpMethodGet, "http://test.stretchr.com/api/v1?~key=ABC123&:name=!Mat&:name=!Laurie&:age=>20", "body", "DIFFERENT-PRIVATE")
+	assert.Equal(t, "34f55c3a086c260098e75066b38ac42e33e8faab", signed)
+
+	signed, _ = getSignature(HttpMethodGet, "http://test.stretchr.com/api/v1?:name=!Laurie&~key=ABC123&:age=>20&:name=!Mat", "body", "DIFFERENT-PRIVATE")
+	assert.Equal(t, "34f55c3a086c260098e75066b38ac42e33e8faab", signed, "Different order of args shouldn't matter")
+
+}
+
+func TestGetSignedURL(t *testing.T) {
+
+	var signed string
+
+	signed, _ = GetSignedURL(HttpMethodGet, "http://test.stretchr.com/api/v1?~key=ABC123&:name=!Mat&:name=!Laurie&:age=>20", "body", "ABC123-private")
+	assert.Equal(t, "http://test.stretchr.com/api/v1?~key=ABC123&:name=!Mat&:name=!Laurie&:age=>20&~sign=df073ee4086eed5848d167871c7424937027728e", signed)
+
+	signed, _ = GetSignedURL(HttpMethodGet, "http://test.stretchr.com/api/v1?~key=ABC123&:name=!Mat&:name=!Laurie&:age=>20", "body", "DIFFERENT-PRIVATE")
+	assert.Equal(t, "http://test.stretchr.com/api/v1?~key=ABC123&:name=!Mat&:name=!Laurie&:age=>20&~sign=34f55c3a086c260098e75066b38ac42e33e8faab", signed)
+
+}
+
+func TestNoBodyHashWhenNoBody(t *testing.T) {
+
+	signed, _ := GetSignedURL(HttpMethodGet, "http://test.stretchr.com/api/v1?~key=ABC123&:name=!Mat&:name=!Laurie&:age=>20", "", "ABC123-private")
+	assert.Equal(t, "http://test.stretchr.com/api/v1?~key=ABC123&:name=!Mat&:name=!Laurie&:age=>20&~sign=bdf49047abf3c8e56de21e244bc24b1c2a6086a2", signed)
+
+}
