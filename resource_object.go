aa5b3e118f5629e66272ab62b6dc7e04919106c9
Mat Ryer <matryer@mac.com>
2012-12-23 12:53:45 -0700

    moved ResourceObject to stretchr package

---
+++ resource_object.go
@@ -0,0 +1,56 @@
+package stretchr
+
+import (
+	"strings"
+)
+
+// ResourceObject represents a general Resource implementation.
+type ResourceObject struct {
+	// Path is the path of this resource.
+	Path string
+	// Data holds the data for the resource.
+	Data map[string]interface{}
+}
+
+// MakeResourceAt makes a new Resource with the specified path.
+func MakeResourceAt(path string) *ResourceObject {
+
+	resource := new(ResourceObject)
+	resource.Path = path
+	resource.Data = make(map[string]interface{})
+
+	// do we need to set the ID in the data?
+	pathSegments := strings.Split(path, pathSeparator)
+	if len(pathSegments)%2 == 0 {
+		// yes -
+		resource.Data[dataFieldID] = pathSegments[len(pathSegments)-1]
+	}
+
+	return resource
+}
+
+// ResourcePath gets the path for this Resource.
+func (r *ResourceObject) ResourcePath() string {
+
+	// break the path apart
+	pathSegments := strings.Split(r.Path, pathSeparator)
+
+	// do we have an ID in the data?
+	if id, hasId := r.Data[dataFieldID]; hasId {
+		// do we have an ID in the path?
+		if len(pathSegments)%2 == 0 {
+			// update the ID
+			pathSegments[len(pathSegments)-1] = id.(string)
+		} else {
+			// add the ID
+			pathSegments = append(pathSegments, id.(string))
+		}
+	}
+
+	return JoinStrings(pathSeparator, pathSegments...)
+}
+
+// ResourceData gets the data for this Resource.
+func (r *ResourceObject) ResourceData() map[string]interface{} {
+	return r.Data
+}
---
+++ resource_object_test.go
@@ -0,0 +1,43 @@
+package stretchr
+
+import (
+	"github.com/stretchrcom/testify/assert"
+	"testing"
+)
+
+func TestResourceObject_Interface(t *testing.T) {
+
+	assert.Implements(t, (*Resource)(nil), new(ResourceObject))
+
+}
+
+func TestMakeResource(t *testing.T) {
+
+	r := MakeResourceAt("people/123")
+	r.Data["name"] = "Mat"
+
+	assert.Equal(t, r.Data[dataFieldID], "123", "MakeResourceAt should set the ID in the data")
+
+	if assert.NotNil(t, r) {
+		assert.Equal(t, "people/123", r.ResourcePath())
+		assert.Equal(t, "Mat", r.ResourceData()["name"])
+	}
+
+}
+
+func TestResourceObject_ResourcePath_GetsIDFromBody(t *testing.T) {
+
+	r := MakeResourceAt("people")
+	r.Data[dataFieldID] = "123"
+
+	assert.Equal(t, "people/123", r.ResourcePath())
+
+}
+func TestResourceObject_ResourcePath_ReplacesIDFromBody(t *testing.T) {
+
+	r := MakeResourceAt("people/abc")
+	r.Data[dataFieldID] = "123"
+
+	assert.Equal(t, "people/123", r.ResourcePath())
+
+}
