69cbe6c5841087271cbd1af83fd9479b377b94b8
Mat Ryer <matryer@mac.com>
2013-01-20 22:49:41 -0700

    added comments to API ChangeInfo

--- api/change_info.go
+++ api/change_info.go
@@ -7,10 +7,15 @@ const (
 	ChangeInfoFieldIDs     string = "~ids"
 )
 
+// ChangeInfo represents a data object containing information about
+// what changed in response to a request.
 type ChangeInfo map[string]interface{}
 
+// NoChangeInfo is a nil ChangeInfo object, and a useful shortcut.
 var NoChangeInfo ChangeInfo = nil
 
+// Created gets the number of records that were created as indicated by
+// this ChangeInfo object.
 func (c ChangeInfo) Created() int {
 	if val, ok := c[ChangeInfoFieldCreated]; ok {
 		return int(val.(float64))
@@ -18,6 +23,8 @@ func (c ChangeInfo) Created() int {
 	return 0
 }
 
+// Updated gets the number of records that were updated as indicated by
+// this ChangeInfo object.
 func (c ChangeInfo) Updated() int {
 	if val, ok := c[ChangeInfoFieldUpdated]; ok {
 		return int(val.(float64))
@@ -25,6 +32,8 @@ func (c ChangeInfo) Updated() int {
 	return 0
 }
 
+// Deleted gets the number of records that were deleted as indicated by
+// this ChangeInfo object.
 func (c ChangeInfo) Deleted() int {
 	if val, ok := c[ChangeInfoFieldDeleted]; ok {
 		return int(val.(float64))
@@ -32,6 +41,8 @@ func (c ChangeInfo) Deleted() int {
 	return 0
 }
 
+// IDs gets the array of (string) IDs that were created in the last
+// request if any.
 func (c ChangeInfo) IDs() []string {
 	ids := []string{}
 	if val, ok := c[ChangeInfoFieldIDs]; ok {
