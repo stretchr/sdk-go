f5a1058f26d822c53faae25ebf59d4418e44f8cc
Tyler Bunnell <tylerbunnell@gmail.com>
2013-03-27 22:18:00 -0600

    Add AddResource

--- resource_collection.go
+++ resource_collection.go
@@ -6,8 +6,15 @@ type ResourceCollection struct {
 
 func MakeResourceCollection(resources []*Resource) *ResourceCollection {
 	c := new(ResourceCollection)
+	if resources == nil {
+		resources = make([]*Resource, 0)
+	}
 	c.Resources = resources
 	return c
+}
+
+func (r *ResourceCollection) AddResource(resource *Resource) {
+	r.Resources = append(r.Resources, resource)
 }
 
 func (r *ResourceCollection) IsEmpty() bool {
